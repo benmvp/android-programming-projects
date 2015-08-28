4d0aa36dd61254b6024a7ca05077eaf5072c89d4
Ben Ilegbodu <benmvp@gmail.com>
2015-08-28 09:29:14 -0700

    Chapter 5 - Your Second Activity. Adding "cheat" activity to get the answers and learning how to pass intents back and forth w/ data

--- GeoQuiz/app/src/main/java/com/benmvp/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/benmvp/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.benmvp.geoquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,8 +15,9 @@ import android.widget.Toast;
 public class QuizActivity extends AppCompatActivity {
     private static final String TAG = "QuizActivity";
     private static final String KEY_INDEX = "index";
+    private static final int REQUEST_CODE_CHEAT = 0;
 
-    private Button mTrueButton, mFalseButton;
+    private Button mTrueButton, mFalseButton, mCheatButton;
     private ImageButton mNextButton, mPreviousButton;
     private TextView mQuestionTextView;
 
@@ -27,6 +29,7 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_asia, true)
     };
     private int mCurrentIndex = 0;
+    private boolean mIsCheater;
 
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
@@ -46,13 +49,15 @@ public class QuizActivity extends AppCompatActivity {
 
     private void checkAnswer(boolean userPressedTrue) {
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
 
-        Toast.makeText(
-                this,
-                userPressedTrue == answerIsTrue
-                    ? R.string.correct_toast : R.string.incorrect_toast,
-                Toast.LENGTH_SHORT
-        ).show();
+        if (mIsCheater)
+            messageResId = R.string.judgment_toast;
+        else
+            messageResId = userPressedTrue == answerIsTrue
+                    ? R.string.correct_toast : R.string.incorrect_toast;
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -89,6 +94,7 @@ public class QuizActivity extends AppCompatActivity {
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                mIsCheater = false;
                 cycleQuestion(true);
             }
         });
@@ -101,10 +107,32 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(QuizActivity.this, answerIsTrue);
+                startActivityForResult(intent, REQUEST_CODE_CHEAT);
+            }
+        });
+
         if (savedInstanceState != null)
             mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
 
         updateQuestion();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != RESULT_OK)
+            return;
+
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null)
+                return;
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
     }
 
     @Override
